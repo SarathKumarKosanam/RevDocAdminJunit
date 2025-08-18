6266f0ae5b263774bcf4bf62e17223b9c4cd0605
SarathKumarKosanam <kosanamsarathkumar@gmail.com>
2025-08-18 23:01:06 +0530

    Initial commit - Selenium Cucumber JUnit framework setup

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,38 +3,39 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public static WebDriver initializeDriver() {
-        if (driver.get() == null) {
-            String browser = System.getProperty("browser", "chrome").toLowerCase();
-            switch (browser) {
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver.set(new FirefoxDriver());
-                    break;
-                case "edge":
-                    WebDriverManager.edgedriver().setup();
-                    driver.set(new EdgeDriver());
-                    break;
-                case "chrome":
-                default:
-                    WebDriverManager.chromedriver().setup();
-                    driver.set(new ChromeDriver());
-                    break;
-            }
-            driver.get().manage().window().maximize();
-        }
+    public static WebDriver getDriver() {
         return driver.get();
     }
 
-    public static WebDriver getDriver() {
-        return driver.get();
+    public static void initDriver() {
+        if (driver.get() == null) {
+            String browser = System.getProperty("browser", "chrome").toLowerCase();
+            boolean isCI = System.getenv("CI") != null; // Detect if running in GitHub Actions
+
+            switch (browser) {
+                case "chrome":
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+
+                    if (isCI) {
+                        // Headless mode for CI/CD
+                        options.addArguments("--headless=new");
+                        options.addArguments("--disable-gpu");
+                        options.addArguments("--no-sandbox");
+                        options.addArguments("--disable-dev-shm-usage");
+                    }
+
+                    driver.set(new ChromeDriver(options));
+                    break;
+            }
+        }
     }
 
     public static void quitDriver() {
